9586c1d6660be1fb58bd69c392607a31f3ccb59c
lixingdeyongyuan <lixingdeyongyuan@gmail.com>
2020-05-25 23:09:09 +0800

    Dialog库

---
+++ dialogmgr/src/main/java/com/aixue/dialogmgr/DialogMgr.kt
@@ -0,0 +1,17 @@
+package com.aixue.dialogmgr
+
+// 作用是什么？为什么这么写？如果写在同一个类中，类会爆炸？不写在同一个类中，又如何实现
+// 可以写出一个共有的类，loading confirm—cancel confirm 弹窗
+// 控制弹窗的弹出顺序
+
+class DialogMgr {
+    // 实例与展示
+
+    fun show(){
+
+    }
+
+    fun dismiss(){
+
+    }
+}
